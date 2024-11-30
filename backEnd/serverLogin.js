337b3f90023268683e65b1266d4f63d64447c2e8
ogedengbesegun <wisdomworld28608@gmail.com>
2024-11-30 11:15:38 +0100

    changes

--- backEnd/serverLogin.js
+++ backEnd/serverLogin.js
@@ -11,25 +11,25 @@ app.use(express.json());
 app.use(cors());
 
 dotenv.config();
-let myURL = "mongodb://localhost:27017/";
+// let myURL = "mongodb://localhost:27017/";
 const PORT2 = process.env.PORT2;
 const client = new MongoClient(myURL);
 client.connect().then(() => {
     const myDB = client.db("tykBank");
     const myCollection = myDB.collection("stds");
 
-    app.post('/getLogin', async (req, res) => {
-        try {
-            const { logid, passwd } = req.body;
-            const getlogin = await myCollection.findOne({ surname: logid, password: passwd })
-            res.json(getlogin);
+    // app.post('/getLogin', async (req, res) => {
+    //     try {
+    //         const { logid, passwd } = req.body;
+    //         const getlogin = await myCollection.findOne({ surname: logid, password: passwd })
+    //         res.json(getlogin);
 
-        }
-        catch (error) {
-            console.log("Check the password")
+    //     }
+    //     catch (error) {
+    //         console.log("Check the password")
 
-        }
-    });
+    //     }
+    // });
 
     app.listen(PORT2, () => {
         console.log(`The server is running on port ${PORT2}`);
